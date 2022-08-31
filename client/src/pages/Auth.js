0c995b694e0739f11a6e5abcf7b67db75f0d8f5a
Roman Grankov <maybe09@mail.ru>
2022-08-31 16:05:29 +0300

    update

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Button, Card, Container, Form, Row } from 'react-bootstrap';
-import { REGISTRATION_ROUTE } from '../utils/constants';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/constants';
 
 export const Auth = () => {
+  const location = useLocation();
+  const isLogin = location.pathname === LOGIN_ROUTE;
+
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
@@ -12,16 +15,24 @@ export const Auth = () => {
       }}
     >
       <Card style={{ width: 600 }} className="p-5">
-        <h2 className="m-auto">Авторизация</h2>
+        <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
         <Form className="d-flex flex-column">
           <Form.Control className="mt-3" placeholder="Введите ваш email..." />
           <Form.Control className="mt-3" placeholder="Введите ваш пароль..." />
           <div className="d-flex justify-content-md-between mt-3">
-            <div>
-              Нет аккаунта?{' '}
-              <NavLink to={REGISTRATION_ROUTE}>Зарегестрируйся!</NavLink>
-            </div>
-            <Button variant="outline-success">Войти</Button>
+            {isLogin ? (
+              <div>
+                Нет аккаунта?{' '}
+                <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+              </div>
+            ) : (
+              <div>
+                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+              </div>
+            )}
+            <Button variant="outline-success">
+              {isLogin ? 'Войти' : 'Регистрация'}
+            </Button>
           </div>
         </Form>
       </Card>
